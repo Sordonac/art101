16daac033462d55c507d22f4b4c845dee8479b24
Sordonac <socrocke@ucsc.edu>
2022-10-18 23:55:22 -0700

    Lab 5 Finished

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Skye Crockett & Matthew Oropeza
+ * Created: 18 October
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Volkswagen";
+ model = "Rabbit";
+ color = "Black";
+ year = 2008;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
